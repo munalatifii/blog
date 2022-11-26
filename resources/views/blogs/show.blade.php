a6e328a9e66088b03ff127d04234f392e4db7660
Muna <munalatifii@gmail.com>
2022-11-26 14:27:59 +0800

    update

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -11,7 +11,7 @@
                     
                     <div class="form-group">
                         <label for="title">Title</label>
-                        <input type="text" class="form-control" id="title" name="title" placeholder="Enter title">
+                        <input type="text" class="form-control" id="title" name="title" placeholder="Enter title" readonly>
                     </div>
                     <div class="form-group">
                         <label for="content">Content</label>
